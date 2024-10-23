97c3b2cea3f70863df5783e11e3bd840f274bcfd
Pratik <erpratikrajverma@gmail.com>
2024-10-23 22:42:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,143 @@
+// let person1 = {
+//     name:'John',
+//     age: 36,
+//     gender: 'male'
+// }
+
+// let person2 = {
+//     name:'Ritika',
+//     age: 26,
+//     gender: 'female'
+// }
+
+// console.log(person1.name)
+
+
+// console.log(person2.name)
+
+
+// person1.name = 'cena'
+
+// delete person1.name
+
+// for(let i in person1){
+//     console.log(person1[i])
+// }
+
+// console.log(person1['name'])
+
+
+// let temp = 30;
+
+// let div = temp;
+
+// div = 40;
+
+// console.log(temp)
+
+// console.log(div)
+
+
+
+
+// let temp = {
+//     value: 30
+// }
+
+// let div = {...temp};
+
+// div.value = 40;
+
+ 
+
+// console.log(temp.value)
+
+// console.log(div.value)
+
+// let obj1 = {
+//     x:1,
+//     y:2,
+//     z:{
+//         a:3,
+//         b:4
+//     }
+// }
+
+// console.log(obj1)
+
+// function makeobject(x,y){
+//     return {
+//         x,
+//         y
+//     }
+// }
+
+// let obj1 = makeobject(1,2)
+
+//  let obj1 = {
+//     x:20,
+//     y:30
+//  }
+
+//  const {x,y} = obj1;
+
+//  console.log(x)
+//  console.log(y)
+
+
+
+
+// ................................( array )...........................
+
+// let array = [1,2,5.5, 'a', 'b', 'pratik'];
+
+// console.log(array);
+
+// for(let i=0 ; i<array.length ; i++ ){
+//     console.log(array[i]);
+// }
+
+// for(let i of array){
+//     console.log(i)
+// }
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+
+
+// arr.pop();
+// arr.push(100);
+
+// console.log(arr)
+
+// console.log(arr.includes(9))
+
+// let array = [
+//     {x:1, y:2},
+//     {x:3, y:4},
+//     {x:5, y:6}
+// ]
+
+// array.forEach( (element)=>{
+//     if(element.x === 10 && element.y === 2){
+//         console.log(true)
+
+//     }else{
+//         console.log(false)
+//     }
+// } )
+
+
+// for(let i=0 ;i<array.length; i++){
+  
+//         if(array[i].x === 1 && array[i].y === 2){
+//             console.log(true)
+//             break
+    
+//         }else if(i == array.length-1 ){
+//             console.log(false)
+//         }
+// }
+
+
+// console.log(array.includes({x:3, y:4}))
+ 
